c8025455e496176cadfff3c1769acf4a4e645335
tonybingo-stack <talentdev423@gmail.com>
2023-12-27 01:36:36 +0200

    tailwind css configure

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <p className="text-lg font-bold">Org, Inc.</p>
+        <p className="text-lg font-bold">hello</p>
+      </div>
+
+    </>
+  );
+};
+
+export default Header;
+
---
+++ src/components/nav.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+        This is header
+    </>
+  );
+};
+
+export default Header;
+
