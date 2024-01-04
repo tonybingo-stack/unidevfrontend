1ff6a5360bccd9d46a35d9167821c61bf95397f6
tonybingo-stack <talentdev423@gmail.com>
2024-01-04 11:43:43 +0200

    new line added with animation according socket message

--- src/components/financecard.tsx
+++ src/components/financecard.tsx
@@ -31,9 +31,8 @@ const FinanceCard = () => {
     socket.emit('message', 'data-for-finance-card');
     // Receive messages from the server
     socket.on('message', (value) => {
-      console.log(`Received message from server: ${value}`);
       const jsonData = JSON.parse(value);
-      setData(jsonData);
+      setData((p) => [...p, ...jsonData]);
       setDataArrived(true);
     });
 
@@ -43,18 +42,45 @@ const FinanceCard = () => {
     };
   }, []);
 
+  const renderCardsInRows = () => {
+    const rows = [];
+    const cardsPerRow = 4;
+
+    for (let i = 4; i < data.length; i += cardsPerRow) {
+      const rowCards = data.slice(i, i + cardsPerRow);
+      const row = (
+        <div className='flex gap-2' key={i}>
+          {rowCards.map((card, index) => (
+            <Card
+              key={index}
+              isDataExist={dataArrived}
+              backgroundcolor={bgColors[index % 4]}
+              percentage={card.percent}
+              shortText={card.shortText}
+              longDesc={card.longDesc}
+            />
+          ))}
+        </div>
+      );
+      rows.push(row);
+    }
+
+    return rows;
+  };
+
   return (
     <>
       <AnimateHeight
         duration={500}
-        height={isShow ? 'auto' : 0}
+        height={isShow ? (data.length < 4 ? 300 : (data.length / 4)*300) : 0}
       >
         <div className={`flex gap-2`}>
-          <Card isDataExist={dataArrived} backgroundcolor={bgColors[0]} percentage={dataArrived ? data[0].percent: 0} shortText={dataArrived ? data[0].shortText : ''} longDesc={dataArrived ? data[0].longDesc : ''} />
-          <Card isDataExist={dataArrived} backgroundcolor={bgColors[1]} percentage={dataArrived ? data[1].percent: 0} shortText={dataArrived ? data[1].shortText : ''} longDesc={dataArrived ? data[1].longDesc : ''} />
-          <Card isDataExist={dataArrived} backgroundcolor={bgColors[2]} percentage={dataArrived ? data[2].percent: 0} shortText={dataArrived ? data[2].shortText : ''} longDesc={dataArrived ? data[2].longDesc : ''} />
-          <Card isDataExist={dataArrived} backgroundcolor={bgColors[3]} percentage={dataArrived ? data[3].percent: 0} shortText={dataArrived ? data[3].shortText : ''} longDesc={dataArrived ? data[3].longDesc : ''} />
+          <Card isDataExist={dataArrived} backgroundcolor={bgColors[0]} percentage={dataArrived ? data[0].percent : 0} shortText={dataArrived ? data[0].shortText : ''} longDesc={dataArrived ? data[0].longDesc : ''} />
+          <Card isDataExist={dataArrived} backgroundcolor={bgColors[1]} percentage={dataArrived ? data[1].percent : 0} shortText={dataArrived ? data[1].shortText : ''} longDesc={dataArrived ? data[1].longDesc : ''} />
+          <Card isDataExist={dataArrived} backgroundcolor={bgColors[2]} percentage={dataArrived ? data[2].percent : 0} shortText={dataArrived ? data[2].shortText : ''} longDesc={dataArrived ? data[2].longDesc : ''} />
+          <Card isDataExist={dataArrived} backgroundcolor={bgColors[3]} percentage={dataArrived ? data[3].percent : 0} shortText={dataArrived ? data[3].shortText : ''} longDesc={dataArrived ? data[3].longDesc : ''} />
         </div>
+        {renderCardsInRows()}
       </AnimateHeight>
     </>
   );
